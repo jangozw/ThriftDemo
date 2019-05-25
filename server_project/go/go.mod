6098819d825b0a754a8ccc070040bb2dd06a815b
jiangge <jiangge@in66.com>
2019-05-25 19:50:23 +0800

    ok

--- server_project/go/go.mod
+++
@@ -1,5 +0,0 @@
-module go-demo
-
-go 1.12
-
-require github.com/apache/thrift v0.12.0
--- server_project/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-
-define('APP_PATH', dirname(__FILE__).'/app');
-define('LOG_PATH', dirname(__FILE__).'/log');
-
-//启动服务,等待客户端调用
-\App\Run::run();
-
